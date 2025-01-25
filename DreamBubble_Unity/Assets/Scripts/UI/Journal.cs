31c5d180969a1c83347603a5f4a7ba3429cc8873
ChicoPacoPancho <rene.cpp@gmail.com>
2025-01-25 11:04:34 -0600

    Add animations to journal UI

--- DreamBubble_Unity/Assets/Scripts/UI/Journal.cs
+++ DreamBubble_Unity/Assets/Scripts/UI/Journal.cs
@@ -1,6 +1,10 @@
+using System.Collections;
 using UnityEngine;
+using DG;
+using DG.Tweening;
+using UnityEngine.EventSystems;
 
-public class Journal : MonoBehaviour
+public class Journal : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     private GameObject m_ItemListPrefab;
@@ -11,6 +15,13 @@ public class Journal : MonoBehaviour
 
     [SerializeField]
     private GameObject m_journalList;
+
+    [SerializeField] private float m_RevealSpeed = 0.5f;
+    [SerializeField] private float m_WriteDelay = 0.25f;
+    [SerializeField] private float m_TimeOnScreen = 3f;
+    [SerializeField] private float m_HideSpeed = 0.65f;
+
+    private bool m_IsRevealed = false;
 
     private void Awake()
     {
@@ -30,18 +41,51 @@ public class Journal : MonoBehaviour
 
     private void Globals_OnDreamItemsChanged()
     {
-        UpdateList();
+        PlayRevealAnimation();
+    }
+
+    private void PlayRevealAnimation(bool updateList = true)
+    {
+        RectTransform rect = transform as RectTransform;
+        float startX = rect.anchoredPosition.x;
+
+        m_IsRevealed = true;
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(rect.DOAnchorPosX(0, m_RevealSpeed).SetEase(Ease.InOutSine));
+        sequence.Insert(m_TimeOnScreen, rect.DOAnchorPosX(startX, m_HideSpeed).SetEase(Ease.InOutSine));
+        sequence.AppendCallback(() => m_IsRevealed = false);
+
+        if (updateList)
+        {
+        sequence.InsertCallback(m_RevealSpeed + m_WriteDelay, UpdateList);
+        }
     }
 
     private void UpdateList()
     {
+        
+
         for (int i = 0; i < Globals.dreamItems.Count; i++)
         {
+            while(m_journalList.transform.childCount > 0)
+            {
+                DestroyImmediate(m_journalList.transform.GetChild(0).gameObject);
+            }
+
             DreamItemData item = Globals.dreamItems[i];
 
             GameObject itemList = Instantiate(m_ItemListPrefab, m_journalList.transform);
             JournalListItem itemListComponent = itemList.GetComponent<JournalListItem>();
             itemListComponent.SetText($"{(i+1)}. {item.journalPhrase}");
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (m_IsRevealed)
+            return;
+
+        PlayRevealAnimation(true);
     }
 }
