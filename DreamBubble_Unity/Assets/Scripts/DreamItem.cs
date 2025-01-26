5f3c42907c614f976153673649fa7c149147241e
ChicoPacoPancho <rene.cpp@gmail.com>
2025-01-26 09:54:06 -0600

    Disable collision on dream objects when they are in bubbles

--- DreamBubble_Unity/Assets/Scripts/DreamItem.cs
+++ DreamBubble_Unity/Assets/Scripts/DreamItem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
@@ -9,6 +10,8 @@ public class DreamItem : MonoBehaviour
     [SerializeField] private float m_BubbleScale = 1.0f;
     [SerializeField] private float m_DreamScale = 1.0f;
 
+    private Collider[] m_Colliders;
+
     private void Start()
     {
         transform.localScale = Vector3.one * m_BubbleScale;
@@ -18,10 +21,22 @@ public class DreamItem : MonoBehaviour
             m_Rigidbody.isKinematic = true;
             m_Rigidbody.useGravity = false;
         }
+
+        m_Colliders = GetComponentsInChildren<Collider>(true);
+        foreach(Collider collider in m_Colliders)
+        {
+            collider.enabled = false;
+        }
     }
 
     public void PlaceInDream()
     {
+
+        foreach(Collider collider in m_Colliders)
+        {
+            collider.enabled = true;
+        }
+
         m_Rigidbody.isKinematic = false;
         m_Rigidbody.useGravity = true;
         m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
